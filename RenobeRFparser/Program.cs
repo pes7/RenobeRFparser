cc55d9b0eb0355076b59aa835002dd90c4b3981b
Назар <wowpes7@mail.ru>
2018-10-27 22:07:33 +0300

    update 0.0.1

--- RenobeRFparser/Program.cs
+++ RenobeRFparser/Program.cs
@@ -9,6 +9,8 @@ using System.Threading;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Newtonsoft.Json.Linq;
+using IronPdf;
+using System.Drawing;
 
 namespace RenobeRFparser
 {
@@ -17,11 +19,41 @@ namespace RenobeRFparser
         static int i = 0;
         static void Main(string[] args)
         {
-            string nextpart = "glava-1-nachinaya-s-segodnyashnego-dnya-ya-nasledniy-prints";
+            Thread th = new Thread(() =>
+            {
+                var _main = new Main();
+                _main.ShowDialog();
+            });
+            th.Start();
+        }
+        static FileStream file;
+        static List<string> glaves;
+
+        public static void Parse()
+        {
+            glaves = new List<string>();
+            string nextpart = "noindex-glava-1-katastrofa-iz-za-banki-koka-koly";
             preloaded_head = File.ReadAllText("preloaded.txt");
+
+            var contenter = urlImage($"https://xn--80ac9aeh6f.xn--p1ai/v1/part/get/?bookAlias=u-menya-est-dom-v-mire-postapokalipsisa&partAlias={nextpart}");
+            using (var client = new WebClient())
+            {
+                client.DownloadFile(contenter["result"]["book"]["image"]["mobile"]["image"].ToString(), "book.jpg");
+            }
+
+            //var f = File.Open("book.png", FileMode.Open);
+            //var img = ReadFully(f);
+
+            IronPdf.HtmlToPdf Renderer = new IronPdf.HtmlToPdf();
+
+            //CREATE PDF
+            //doc.AddAuthor("Nazar Ukolov conventor");
+            //doc.AddImageFile("book.png","book.epub");
+            //doc.AddTitle(contenter["result"]["book"]["title"].ToString());
+            string _name = "";
             while (nextpart != null)
             {
-                HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://xn--80ac9aeh6f.xn--p1ai/v1/part/load/?bookAlias=releas-that-witch&partAlias={nextpart}");
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"https://xn--80ac9aeh6f.xn--p1ai/v1/part/get/?bookAlias=u-menya-est-dom-v-mire-postapokalipsisa&partAlias={nextpart}");
                 JArray array = new JArray();
                 using (var twitpicResponse = (HttpWebResponse)request.GetResponse())
                 using (var reader = new StreamReader(twitpicResponse.GetResponseStream()))
@@ -30,6 +62,8 @@ namespace RenobeRFparser
 
                     JObject joResponse = JObject.Parse(objText);
                     var name = joResponse["result"]["book"]["title"];
+                    if (_name.Length < 1)
+                        _name = name.ToString();
                     if (!Directory.Exists(name.ToString()))
                         Directory.CreateDirectory(name.ToString());
                     var title = joResponse["result"]["part"]["title"];
@@ -46,7 +80,7 @@ namespace RenobeRFparser
                     try
                     {
                         string path;
-                        if (title.ToString().Split('.').Length>1)
+                        if (title.ToString().Split('.').Length > 1)
                             path = $"{ name.ToString() }/{CleanFileName(title.ToString().Split('.').First())} { CleanFileName(title.ToString().Split('.').ElementAt(1))}.html";
                         else
                             path = $"{ name.ToString() }/{CleanFileName(title.ToString().Split('.').First())}.html";
@@ -58,18 +92,96 @@ namespace RenobeRFparser
                             }
                             catch { }
                         }
-                        file = File.Open(path, FileMode.Create);
-                        d.Save(file);
-                        file.Close();
+                        //file = File.Open(path, FileMode.Create);
+                        //d.Save(file);
+                        //file.Close();
+
+                        string _content = $"<head>{preloaded_head}<title>{title.ToString()}</title></head><body><h2>{title.ToString()}</h2><br> {content.ToString()}";
+                        string dir = $"{name.ToString()}/{CleanFileName(title.ToString().Split('.').ElementAt(1))}.pdf";
+                        Renderer.RenderHtmlAsPdf(_content).SaveAs(dir);
+                        glaves.Add(dir);
                         Console.WriteLine($"{path} --- Loaded!");
-                        char[] delimiters = new char[] { ' ', '\r', '\n' };
-                        i += StripHTML(content.ToString()).Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length;
+                        Thread.Sleep(500);
                     }
                     catch { Console.WriteLine($"------------------------------------------ Error!"); }
                 }
             }
+            SavePDF(_name);
         }
-        static FileStream file;
+
+        static List<string> glaves50;
+        public static void SavePDF(string _name)
+        {
+            
+            glaves50 = new List<string>();
+            /*
+            var files = Directory.GetFiles($"{_name}", "*.pdf").OrderBy(d => new FileInfo(d).CreationTime);
+            foreach (var file in files)
+            {
+                PDFs.Add(PdfDocument.FromFile(file));
+            }
+            */
+            int i = 0, k = 0;
+            string path = $"Сводка/{_name}";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            glaves50.AddRange(Directory.GetFiles($"{_name}", "*.pdf").OrderBy(d => new FileInfo(d).CreationTime));
+            var PDFs = new List<PdfDocument>();
+            foreach (var file in glaves50)
+            {
+                Console.WriteLine(file);
+                PDFs.Add(PdfDocument.FromFile(file));
+                if (i >= 50)
+                {
+                    PdfDocument PDFer = PdfDocument.Merge(PDFs);
+                    PDFer.SaveAs($"{path}/{_name}_{k}.pdf");
+                    k++;
+                    i = 0;
+                    PDFs.Clear();
+                }
+                i++;
+            }
+            if(PDFs.Count > 0)
+            {
+                PdfDocument PDFer = PdfDocument.Merge(PDFs);
+                PDFer.SaveAs($"{path}/{_name}_{k}.pdf");
+            }
+            PDFs.Clear();
+            var list = new List<string>();
+            list.AddRange(Directory.GetFiles($"{Directory.GetCurrentDirectory()}/{path}","*.pdf").OrderBy(d => new FileInfo(d).CreationTime));
+            foreach(var f in list)
+            {
+                PDFs.Add(PdfDocument.FromFile(f));
+            }
+            PdfDocument PDF = PdfDocument.Merge(PDFs);
+            var image = Image.FromFile("book.jpg");
+            var cover = ImageToPdfConvetrer.ImageToPdf(image);
+            PDF.PrependPdf(cover);
+            PDF.SaveAs($"{_name}.pdf");
+        }
+
+        public static byte[] ReadFully(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        public static JObject urlImage(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"{url}");
+            JArray array = new JArray();
+            using (var twitpicResponse = (HttpWebResponse)request.GetResponse())
+            using (var reader = new StreamReader(twitpicResponse.GetResponseStream()))
+            {
+                var objText = reader.ReadToEnd();
+
+                JObject joResponse = JObject.Parse(objText);
+                return joResponse;
+            }
+        }
 
         public static string StripHTML(string input)
         {
